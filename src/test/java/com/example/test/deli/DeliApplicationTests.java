94289c1ebd3a34c3ca62d3860921e2aae6155ca4
Alberto <albertojunior848423803@gmail.com>
2024-12-24 11:11:14 +0300

    first commit

---
+++ src/test/java/com/example/test/deli/DeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.deli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
